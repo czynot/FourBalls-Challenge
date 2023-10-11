fd0d1eba858c7b4b7f67d8d820bd35ca4e81f9bb
Parth Purvesh <parth29dec@gmail.com>
2023-10-11 22:27:08 +0530

    Object Oriented solution added

--- ObjectOriented/src/main/java/Ball.java
+++ ObjectOriented/src/main/java/Ball.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class Ball {
+import processing.core.PApplet;
+public class Ball extends PApplet {
+
+    int xPosition;
+    int yPosition;
+    int speed;
+    int diameter;
+    PApplet obj;
+
+    Ball(int xPosition, int yPosition, int speed, int diameter, PApplet obj)
+    {
+        this.xPosition = xPosition;
+        this.yPosition = yPosition;
+        this.speed = speed;
+        this.diameter = diameter;
+        this.obj = obj;
+    }
+
+    void display()
+    {
+        obj.ellipse(xPosition, yPosition, diameter, diameter);
+    }
+
+    void move()
+    {
+        xPosition += speed;
+    }
 }
